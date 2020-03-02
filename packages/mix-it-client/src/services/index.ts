407a0491f4d02409c71832e9e6d1804e211178c2
Andrea Gueugnaut <agueugnaut@octo.com>
2020-03-02 14:49:03 +0100

    feat(services): add party service

--- packages/mix-it-client/src/services/index.ts
+++ packages/mix-it-client/src/services/index.ts
@@ -1,8 +1,10 @@
 import HostService from '@/services/host.service'
+import { createPartyService } from '@/services/party.service'
 
 const hostUrl = 'http://localhost:3000'
 
-const hostService = new HostService({ hostApi: hostUrl })
+export const hostService = new HostService({ hostApi: hostUrl })
+export const partyService = createPartyService({ baseURL: hostUrl })
 
 // eslint-disable-next-line import/prefer-default-export
-export { hostService }
+export default { hostService, partyService }
