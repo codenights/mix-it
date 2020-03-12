5f9c65e8e5253a0a11b1b849c12bca5f0939cc46
Andrea Gueugnaut <agueugnaut@octo.com>
2020-03-12 09:51:47 +0100

    refactor(main): remove Vue-Socket-IO import

--- packages/mix-it-client/src/main.ts
+++ packages/mix-it-client/src/main.ts
@@ -2,19 +2,11 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueCompositionApi from '@vue/composition-api'
-import socketio from 'socket.io-client'
-import VueSocketIO from 'vue-socket.io'
 import GAuth from 'vue-google-oauth2'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-const SocketInstance = socketio.connect('http://localhost:3000/parties')
-
-Vue.use(new VueSocketIO({
-  debug: true,
-  connection: SocketInstance
-}))
 const gauthOption = {
   clientId: '298958821124-70r9qplggm5je4d3jhqt9n7j927v5n65.apps.googleusercontent.com',
   scope: 'profile email',
