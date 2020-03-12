e22e5547efa0e41e997ecfae0cd903392456e0e1
Andrea Gueugnaut <agueugnaut@octo.com>
2020-03-12 10:13:47 +0100

    feat(config): allow to configure the API base URL

--- packages/mix-it-front/src/services/index.ts
+++ packages/mix-it-front/src/services/index.ts
@@ -1,11 +1,10 @@
 import HostService from '@/services/host.service'
 import RoomService from '@/services/room.service'
 import { createPartyService } from '@/services/party.service'
+import config from '@/config'
 
-const hostUrl = 'http://localhost:3000'
-
-export const hostService = new HostService({ hostApi: hostUrl })
-export const roomService = new RoomService({ hostApi: hostUrl })
-export const partyService = createPartyService({ baseURL: hostUrl })
+export const hostService = new HostService({ hostApi: config.api })
+export const roomService = new RoomService({ hostApi: config.api })
+export const partyService = createPartyService({ baseURL: config.api })
 
 export default { hostService, roomService, partyService }
