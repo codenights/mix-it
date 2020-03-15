10bd8bc684964a34048fc4a822d8c08e90a4f085
Andrea Gueugnaut <agueugnaut@octo.com>
2020-03-15 21:40:20 +0100

    test(e2e): test joining party

--- packages/mix-it-client/tests/e2e/support/index.js
+++ packages/mix-it-client/tests/e2e/support/index.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './party'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
