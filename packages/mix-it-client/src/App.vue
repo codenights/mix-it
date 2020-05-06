6f30074c2f0009e302b81416e6fa2acc79fcbb56
jordan.taillefer <jordan.taillefer@accenture.com>
2020-05-06 22:30:46 +0200

    ci: clear travis.yml

--- packages/mix-it-client/src/App.vue
+++ packages/mix-it-client/src/App.vue
@@ -8,32 +8,32 @@
 </template>
 
 <script>
-    import { defineComponent } from '@vue/composition-api'
-    import MainLayout from './views/MainLayout/MainLayout.vue'
+import { defineComponent } from '@vue/composition-api'
+import MainLayout from './views/MainLayout/MainLayout.vue'
 
-    const App = defineComponent({
-        name: 'App',
-        components: { MainLayout },
-    })
+const App = defineComponent({
+  name: 'App',
+  components: { MainLayout }
+})
 
-    export default App
+export default App
 </script>
 
 <style lang="scss">
-  body {
-    margin: 0;
-  }
-  .nav {
-    height: 4rem;
-    padding: 1rem;
-  }
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background: linear-gradient(14.46deg, #b3e5fc 22.91%, #bbdefb 65.88%, #81d4fa 100.49%);
-  }
-  * {
-    box-sizing: border-box;
-  }
+body {
+  margin: 0;
+}
+.nav {
+  height: 4rem;
+  padding: 1rem;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: linear-gradient(14.46deg, #b3e5fc 22.91%, #bbdefb 65.88%, #81d4fa 100.49%);
+}
+* {
+  box-sizing: border-box;
+}
 </style>
